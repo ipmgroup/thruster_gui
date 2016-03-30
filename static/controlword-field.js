2cc4f2e92f9856a92f8c387d7a75c0a3cd34172f
Dmitri Ranfft <dmitri.ranfft@evologics.de>
2016-03-30 14:58:07 +0200

    Implemented controlword field front end.

--- static/controlword-field.js
+++ static/controlword-field.js
@@ -13,6 +13,12 @@
         self._flags = flags;
         self._fnames = Object.keys(flags);
 
+        self._legend = null;
+        self._checkboxes = null;
+        self._cblabels = null;
+        self._input = null;
+        self._button = null;
+
         self._genField();
         self._attachHandler(handler);
     }
@@ -27,7 +33,7 @@
 
     ControlwordField.prototype._genCheckboxes = function(){
         var self = this;
-        var field = self.content;
+        //var field = self.content;
         var checkboxes = self._checkboxes = self._fnames.map(function(name){
             var el = mkInput("checkbox", name);
             //console.log("genCheckbox", el);
@@ -37,14 +43,36 @@
         //console.log("generated_checkboxes", self._checkboxes);
     }
 
+    ControlwordField.prototype._genCbLabels = function(){
+        var self = this;
+        //var field = self.content;
+        var labels = self._cblabels = self._fnames.map(function(name){
+            var el = mk("label");
+            el.textContent = self._flags[name].name;
+            return el;
+        });
+    }
+
     ControlwordField.prototype._genField = function(){
         var self = this;
         self._genCheckboxes();
+        self._genCbLabels();
         var checkboxes = self._checkboxes;
-        //console.log("checkboxes", self._checkboxes);
-        checkboxes.forEach(function(cb){
-            self.content.appendChild(cb);
-        });
+        var labels = self._cblabels;
+        var legend = self._legend = mk("legend");
+        legend.textContent = "Control Word";
+        self.content.appendChild(legend);
+        for(var i = 0; i < Object.keys(checkboxes).length && i < Object.keys(labels).length; i ++){
+            labels[i].insertBefore(checkboxes[i], labels[i].firstChild)
+            self.content.appendChild(labels[i]);
+            self.content.appendChild(mk("br"));
+        }
+        var input = self._input = mkInput("text", "cwinput");
+        input.value = "0x0000";
+        self.content.appendChild(input);
+        var button = self._button = mkInput("button", "cwbutton");
+        button.value = "Apply";
+        self.content.appendChild(button);
     }
 
     ControlwordField.prototype._attachHandler = function(handler){
